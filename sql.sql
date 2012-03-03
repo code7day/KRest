cd110d83490f5851f7039711fa2189bfc2e2f09a
Alberto <aberoteran@live.com>
2012-03-03 18:21:34 -0430

    Actualizado el sql

--- sql.sql
+++ sql.sql
@@ -1,14 +1,9 @@
-
 CREATE TABLE IF NOT EXISTS `books` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `author` varchar(30) COLLATE utf8_bin NOT NULL,
   `title` varchar(20) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=18 ;
-
---
--- Volcado de datos para la tabla `books`
---
 
 INSERT INTO `books` (`id`, `author`, `title`) VALUES
 (1, 'Bram Stoke', 'Drácula'),
